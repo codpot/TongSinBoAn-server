d09b35e35a53f935f991e331ad176ba44a343a1b
Seungwan Seo <wan9824@naver.com>
2017-10-17 16:30:19 +0900

    Add group create and join API

--- db/group.js
+++ db/group.js
@@ -1,7 +1,7 @@
 var db = require('./index');
 
 // 그룹 생성
-exports.groupCreate = function (name, admin_idx, callback) {
+exports.create = function (name, admin_idx, callback) {
   db.query("INSERT INTO `group` (`name`, `admin_idx`) VALUES (?, ?);", [name, admin_idx], function (error, results, fields) {
     if (!error) {
       callback(results.insertId);
@@ -12,8 +12,19 @@ exports.groupCreate = function (name, admin_idx, callback) {
 };
 
 // 그룹 수정
-exports.groupUpdate = function (idx, value, callback) {
+exports.update = function (idx, value, callback) {
   db.query("UPDATE `group` SET ? WHERE `idx`=?;", [value, idx], function (error, results, fields) {
+    if (!error) {
+      callback(Boolean(results.affectedRows));
+    } else {
+      callback(false);
+    }
+  });
+};
+
+// 그룹 할당
+exports.assign = function (member_idx, group_idx, level, ok, callback) {
+  db.query("UPDATE `member` SET `group_idx`=?, `level`=?, `group_ok`=? WHERE  `idx`=?;", [group_idx, level, ok, member_idx], function (error, results, fields) {
     if (!error) {
       callback(Boolean(results.affectedRows));
     } else {
--- routes/group.js
+++ routes/group.js
@@ -1,4 +1,58 @@
 var express = require('express');
+var group = require('../db/group');
+var member = require('../db/member');
 var router = express.Router();
+
+// [함수] 그룹 생성
+var group_create = function (res, name, member_idx) {
+  group.create(name, member_idx, function (group_idx) {
+    if (group_idx) {
+      group_join(res, member_idx, group_idx, 3, 1);
+    } else {
+      res.json({'result': false, 'msg': 'group_create_failed'});
+    }
+  });
+};
+
+// [함수] 그룹 가입
+var group_join = function (res, member_idx, group_idx, level, ok) {
+  group.assign(member_idx, group_idx, level, ok, function (i_result) {
+    if (i_result) {
+      res.json({'result': true, 'msg': 'success'});
+    } else {
+      res.json({'result': false, 'msg': 'group_join_failed'});
+    }
+  });
+};
+
+// 그룹 생성
+router.post('/', function (req, res) {
+  if (req.session.member_idx) {
+    member.read(req.session.member_idx, function (result, data) {
+      if (data[0]['level'] === 0) {
+        group_create(res, req.body.name, req.session.member_idx);
+      } else {
+        res.json({'result': false, 'msg': 'already_have_group'});
+      }
+    });
+  } else {
+    res.json({'result': false, 'msg': 'login_required'});
+  }
+});
+
+// 그룹 가입
+router.post('/join', function (req, res) {
+  if (req.session.member_idx) {
+    member.read(req.session.member_idx, function (result, data) {
+      if (data[0]['level'] === 0) {
+        group_join(res, req.session.member_idx, req.body.group, 1, 0);
+      } else {
+        res.json({'result': false, 'msg': 'already_have_group'});
+      }
+    });
+  } else {
+    res.json({'result': false, 'msg': 'login_required'});
+  }
+});
 
 module.exports = router;
