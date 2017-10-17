858b978a242337b8b3909254e7c77a0e954a6072
Seungwan Seo <wan9824@naver.com>
2017-10-17 21:00:33 +0900

    Add Policy verify API

--- db/policy.js
+++ db/policy.js
@@ -21,3 +21,25 @@ exports.update = function (policy_idx, name, comment, mdm, callback) {
     }
   });
 };
+
+// 정책 검증 권한 확인
+exports.check_auth = function (policy_idx, member_idx, callback) {
+  db.query("SELECT * FROM `policy_admin` WHERE `policy_idx` = ? AND `member_idx` = ?;", [policy_idx, member_idx], function (error, results, fields) {
+    if (!error && results.length === 1) {
+      callback(true);
+    } else {
+      callback(false);
+    }
+  });
+};
+
+// 정책 검증
+exports.verify = function (policy_idx, token, now, callback) {
+  db.query("SELECT p.* FROM `policy_user` as p WHERE p.policy_idx = ? AND p.member_idx = (SELECT m.idx FROM `member` as m WHERE m.token=? AND m.token_valid >= ?);", [policy_idx, token, now], function (error, results, fields) {
+    if (!error && results.length === 1) {
+      callback(true);
+    } else {
+      callback(false);
+    }
+  });
+};
--- routes/policy.js
+++ routes/policy.js
@@ -1,6 +1,19 @@
 var express = require('express');
+var moment = require('moment');
 var policy = require('../db/policy');
 var router = express.Router();
+
+// [함수] 출입증 검증
+var policy_verify = function (res, policy_idx, token) {
+  var now = moment().format('YYYY-MM-DD HH:mm:ss');
+  policy.verify(policy_idx, token, now, function (result) {
+    if (result) {
+      res.json({'result': true, 'msg': 'success'});
+    } else {
+      res.json({'result': false, 'msg': 'no_permission'});
+    }
+  });
+};
 
 // 정책 생성
 router.post('/', function (req, res) {
@@ -15,9 +28,24 @@ router.post('/', function (req, res) {
 
 // 정책 수정
 router.put('/:policy_idx', function (req, res) {
-  if (req.session.member_idx && req.session.level == 3) {
+  if (req.session.member_idx && req.session.level === 3) {
     policy.update(req.params.policy_idx, req.body.name, req.body.comment, req.body.mdm, function (result, msg) {
       res.json({'result': result, 'msg': msg});
+    });
+  } else {
+    res.json({'result': false, 'msg': 'login_required'});
+  }
+});
+
+// 정책 검증
+router.post('/verify/:policy_idx', function (req, res) {
+  if (req.session.member_idx && req.session.level >= 2) {
+    policy.check_auth(req.params.policy_idx, req.session.member_idx, function (result) {
+      if (result) {
+        policy_verify(res, req.params.policy_idx, req.body.token);
+      } else {
+        res.json({'result': false, 'msg': 'login_required'});
+      }
     });
   } else {
     res.json({'result': false, 'msg': 'login_required'});
