ac17417c35617fb96b304db92d54ccead82a2a14
Seungwan Seo <wan9824@naver.com>
2017-10-18 14:25:47 +0900

    Add option to change database port

--- db/index.js
+++ db/index.js
@@ -2,6 +2,7 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit: process.env.DB_POOL,
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_DATABASE
