58631532b478c0e3ad61f7cff2bf1878ef4a528e
Seungwan Seo <wan9824@naver.com>
2017-10-20 06:31:45 +0900

    Fix bug when create group

--- routes/group.js
+++ routes/group.js
@@ -8,7 +8,7 @@ router.post('/', function (req, res) {
   if (req.session.member_idx) {
     member.read_idx(req.session.member_idx, function (result, data) {
       if (data[0]['level'] === 0) {
-        group_create(res, req.body.name, req.session.member_idx);
+        group_create(req, res, req.body.name, req.session.member_idx);
       } else {
         res.json({'result': false, 'msg': 'group_exists'});
       }
@@ -23,7 +23,7 @@ router.post('/join', function (req, res) {
   if (req.session.member_idx) {
     member.read_idx(req.session.member_idx, function (result, data) {
       if (data[0]['level'] === 0) {
-        group_join(res, req.session.member_idx, req.body.group_idx, 0);
+        group_join(req, res, req.session.member_idx, req.body.group_idx, 0);
       } else {
         res.json({'result': false, 'msg': 'group_exists'});
       }
@@ -34,10 +34,10 @@ router.post('/join', function (req, res) {
 });
 
 // # 그룹 생성
-var group_create = function (res, name, member_idx) {
+var group_create = function (req, res, name, member_idx) {
   group.create(name, member_idx, function (group_idx) {
     if (group_idx) {
-      group_join(res, member_idx, group_idx, 3);
+      group_join(req, res, member_idx, group_idx, 3);
     } else {
       res.json({'result': false, 'msg': 'group_create_failed'});
     }
@@ -45,9 +45,11 @@ var group_create = function (res, name, member_idx) {
 };
 
 // # 그룹 가입
-var group_join = function (res, member_idx, group_idx, level) {
+var group_join = function (req, res, member_idx, group_idx, level) {
   member.update(member_idx, {'group_idx': group_idx, 'level': level}, function (i_result) {
     if (i_result) {
+      req.session.group_idx = group_idx;
+      req.session.level = level;
       res.json({'result': true});
     } else {
       res.json({'result': false, 'msg': 'group_join_failed'});
